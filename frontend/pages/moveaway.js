93733f2e7ff7c0dc9487c4a52b6fda8d37e54fbd
Teeramaet Bongkodmalee <31606001+teeramaet@users.noreply.github.com>
2022-11-21 03:48:08 +0700

    add secret door

---
+++ frontend/pages/moveaway.js
@@ -0,0 +1,15 @@
+var caller = $("#emcee");
+
+function foo() {
+  var randX = Math.floor(Math.random() * (window.innerWidth - 100));
+  var randY = Math.floor(Math.random() * (window.innerHeight - 100));
+  console.log([randX, randY]);
+  caller.stop().animate({ left: randX + "px", top: randY + "px" });
+}
+
+$(document).ready(function () {
+  caller.on("mouseenter", foo);
+  caller.on("click", function () {
+    window.location.href = "loginPagead.html";
+  });
+});
